1150307512034eb012101e518594695fc06370e5
Authapon Kongwan <authapon@gmail.com>
2024-09-18 12:45:19 +0700

    change to unix time

--- go.mod
+++ go.mod
@@ -2,18 +2,19 @@ module github.com/ruts48code/otp4ruts
 
 go 1.22.1
 
-require github.com/ruts48code/utils4ruts v0.0.0-20240327014149-65e8d90f196f
+require github.com/ruts48code/utils4ruts v0.0.0-20240918052915-101ffdc75b7f
 
 require (
 	github.com/agext/levenshtein v1.2.3 // indirect
 	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
-	github.com/hashicorp/hcl/v2 v2.20.1 // indirect
+	github.com/hashicorp/hcl/v2 v2.22.0 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
 	github.com/ruts48code/random4ruts v0.0.0-20240318160157-d85939eda018 // indirect
-	github.com/zclconf/go-cty v1.14.4 // indirect
-	golang.org/x/mod v0.16.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	golang.org/x/tools v0.19.0 // indirect
+	github.com/zclconf/go-cty v1.15.0 // indirect
+	golang.org/x/mod v0.21.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	golang.org/x/tools v0.25.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package otp4ruts
 import (
 	"crypto/hmac"
 	"crypto/sha256"
-	"crypto/sha512"
-	"encoding/binary"
 	"encoding/hex"
 	"time"
 
@@ -24,7 +22,7 @@ func OTP256Hex(text []byte, key []byte) string {
 }
 
 func TimeOTP256Hex(key []byte) string {
-	return hex.EncodeToString(HmacOUT256(key, []byte(utils.GetTimeStamp(time.Now()))))
+	return hex.EncodeToString(HmacOUT256(key, []byte(utils.GetUnixTime(time.Now()))))
 }
 
 func ChkOTP256Hex(text []byte, key []byte, chk string) bool {
@@ -35,84 +33,7 @@ func ChkTimeOTP256Hex(key []byte, chk string, timerange int) bool {
 	t := time.Now()
 	for i := (-1 * timerange); i <= timerange; i++ {
 		tx := t.Add(time.Duration(i) * time.Second)
-		if OTP256Hex([]byte(utils.GetTimeStamp(tx)), key) == chk {
-			return true
-		}
-	}
-	return false
-}
-
-// Standard OTP
-
-func HmacOUT(secret []byte, data []byte) []byte {
-	h := hmac.New(sha512.New, secret)
-	h.Write(data)
-	return h.Sum(nil)
-}
-
-func OTP(secret []byte, data []byte) uint64 {
-	return binary.LittleEndian.Uint64(HmacOUT(secret, data)[:8])
-}
-
-func OTPxMOD(secret []byte, data []byte, mod uint64) uint64 {
-	return OTP(secret, data) % mod
-}
-
-func OTPxHex(secret []byte, data []byte, size int) string {
-	return hex.EncodeToString(HmacOUT(secret, data)[:size])
-}
-
-func ChkOTP(secret []byte, data []byte, chk uint64) bool {
-	return OTP(secret, data) == chk
-}
-
-func ChkOTPxMOD(secret []byte, data []byte, mod uint64, chk uint64) bool {
-	return OTPxMOD(secret, data, mod) == chk
-}
-
-func ChkOTPxHex(secret []byte, data []byte, size int, chk string) bool {
-	return OTPxHex(secret, data, size) == chk
-}
-
-func TimeOTP(data []byte) uint64 {
-	return OTP([]byte(utils.GetTimeStamp(time.Now())), data)
-}
-
-func TimeOTPxMOD(data []byte, mod uint64) uint64 {
-	return OTPxMOD([]byte(utils.GetTimeStamp(time.Now())), data, mod)
-}
-
-func TimeOTPxHex(data []byte, size int) string {
-	return OTPxHex([]byte(utils.GetTimeStamp(time.Now())), data, size)
-}
-
-func ChkTimeOTP(data []byte, chk uint64, timerange int) bool {
-	t := time.Now()
-	for i := (-1 * timerange); i <= timerange; i++ {
-		tx := t.Add(time.Duration(i) * time.Second)
-		if OTP([]byte(utils.GetTimeStamp(tx)), data) == chk {
-			return true
-		}
-	}
-	return false
-}
-
-func ChkTimeOTPxMOD(data []byte, mod uint64, chk uint64, timerange int) bool {
-	t := time.Now()
-	for i := (-1 * timerange); i <= timerange; i++ {
-		tx := t.Add(time.Duration(i) * time.Second)
-		if OTPxMOD([]byte(utils.GetTimeStamp(tx)), data, mod) == chk {
-			return true
-		}
-	}
-	return false
-}
-
-func ChkTimeOTPxHex(data []byte, size int, chk string, timerange int) bool {
-	t := time.Now()
-	for i := (-1 * timerange); i <= timerange; i++ {
-		tx := t.Add(time.Duration(i) * time.Second)
-		if OTPxHex([]byte(utils.GetTimeStamp(tx)), data, size) == chk {
+		if OTP256Hex([]byte(utils.GetUnixTime(tx)), key) == chk {
 			return true
 		}
 	}
